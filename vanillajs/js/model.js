c0c66068afc4c933d514e30c90e5ea4db7fdddb2
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-05 09:49:59 -0200

    Add new cat

--- vanillajs/js/model.js
+++ vanillajs/js/model.js
@@ -4,9 +4,10 @@
 var model = {
     cats: [
         { name: 'Pixel', src: 'cat-01', count: 5 },
-        { name: 'Davi', src: 'cat-02', count: 1 },
-        { name: 'Jerry', src: 'cat-03', count: 3 },
-        { name: 'Tom', src: 'cat-04', count: 0 }
+        { name: 'Garfield', src: 'cat-02', count: 1 },
+        { name: 'Bichento', src: 'cat-03', count: 3 },
+        { name: 'Tom', src: 'cat-04', count: 0 },
+        { name: 'Buttercup', src: 'cat-05', count: 3 },
     ],
 
     currentCat: 0,
