79ee68c3288e6be02e3e624b295063f30ef3562a
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-07 04:36:36 -0200

    add initial cats array

--- knockoutjs/model.js
+++ knockoutjs/model.js
@@ -21,3 +21,11 @@ var Cat = function(info) {
 		return t;
 	}, this);
 };
+
+var initialCats = [
+	{ name: 'Pixel', src: 'img/cat-01.jpg', count: 5, nicks: ['nick01', 'nick02', 'nick03'] },
+    { name: 'Garfield', src: 'img/cat-02.jpg', count: 1, nicks: ['nick04', 'nick05', 'nick06'] },
+    { name: 'Bichento', src: 'img/cat-03.jpg', count: 3, nicks: ['nick07', 'nick08', 'nick09'] },
+    { name: 'Tom', src: 'img/cat-04.jpg', count: 0, nicks: ['nick10', 'nick11', 'nick12'] },
+    { name: 'Buttercup', src: 'img/cat-05.jpg', count: 3, nicks: ['nick12', 'nick13', 'nick14'] }
+];
