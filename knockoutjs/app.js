27ad395559a94864399916a0eae90ba29d6b2aa2
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-07 03:33:48 -0200

    separate concerns

--- knockoutjs/app.js
+++ knockoutjs/app.js
@@ -1,32 +1,13 @@
 var app = function() {
 
-	this.name  = ko.observable('Pixel');
-	this.count = ko.observable(4);
-	this.image = ko.observable('img/cat-01.jpg');
-	this.nicks = ko.observableArray(['Pix', 'Sweet', 'El', 'Mr. P.']);
-
-	this.level = ko.computed(function() {
-		var n = this.count(),
-			r = 'baby';
-
-		if ( n >= 10 && n < 25 ) {
-			r = 'teen';
-		}
-		else if ( n >= 25 && n < 50 ) {
-			r = 'adult';
-		}
-		else if ( n >= 50 ) {
-			r = 'grandpa';
-		}
-
-		return r;
-
-	}, this);
+	this.currentCat = ko.observable( new Cat() );
 
 	this.increment = function() {
-		this.count( this.count() + 1 );
+		var cat = this.currentCat();
+
+		cat.count( cat.count() + 1 );
 	};
 
 };
 
-ko.applyBindings(new app());
+ko.applyBindings( new app() );
---
+++ knockoutjs/model.js
@@ -0,0 +1,24 @@
+var Cat = function() {
+	this.name  = ko.observable('Pixel');
+	this.count = ko.observable(4);
+	this.image = ko.observable('img/cat-01.jpg');
+	this.nicks = ko.observableArray(['Pix', 'Sweet', 'El', 'Mr. P.']);
+
+	this.level = ko.computed(function() {
+		var n = this.count(),
+			r = 'baby';
+
+		if ( n >= 10 && n < 25 ) {
+			r = 'teen';
+		}
+		else if ( n >= 25 && n < 50 ) {
+			r = 'adult';
+		}
+		else if ( n >= 50 ) {
+			r = 'grandpa';
+		}
+
+		return r;
+
+	}, this);
+};
