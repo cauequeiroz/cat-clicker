4f04d579781888eb4d58e5368d4d20342b40a3df
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-07 06:42:22 -0200

    Create and load list of cats

--- knockoutjs/js/app.js
+++ knockoutjs/js/app.js
@@ -7,7 +7,13 @@
 var App = function() {
     var $this = this;
 
+    $this.catList = ko.observableArray([]);
     
+    initialCats.forEach(function(cat_data) {
+        $this.catList.push( new Cat(cat_data) );
+    });
+
+    $this.currentCat = ko.observable( $this.catList()[0] );
 };
 
 ko.applyBindings( new App() );
--- knockoutjs/js/model.js
+++ knockoutjs/js/model.js
@@ -1,3 +1,16 @@
 /* ==========================
     Model
    ========================== */
+var Cat = function(data) {
+    this.name  = ko.observable(data.name);
+    this.count = ko.observable(data.count);
+    this.image = ko.observable(data.src);
+};
+
+var initialCats = [
+    { name: 'Pixel', src: 'img/cat-01.jpg', count: 5 },
+    { name: 'Garfield', src: 'img/cat-02.jpg', count: 1 },
+    { name: 'Bichento', src: 'img/cat-03.jpg', count: 3 },
+    { name: 'Tom', src: 'img/cat-04.jpg', count: 0 },
+    { name: 'Buttercup', src: 'img/cat-05.jpg', count: 3 },
+];
