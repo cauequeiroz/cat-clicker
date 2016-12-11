3ae840e18c371ed020094cbb1b18c81a15d77f0a
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-11 08:37:54 -0200

    Create module

---
+++ angularjs/js/app.js
@@ -0,0 +1,9 @@
+/* -------------------------------------------
+
+	Cat Clicker
+	@AngularJS Version
+	@Developed by Caue Queiroz	
+
+-------------------------------------------- */
+angular
+	.module('CatClicker', []);
