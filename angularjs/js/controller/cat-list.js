72db1f14af26e41a1d7824e37c0fc2ac19f24fb4
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-11 08:55:04 -0200

    generate cat list

--- angularjs/js/controller/cat-list.js
+++ angularjs/js/controller/cat-list.js
@@ -7,6 +7,12 @@
 -------------------------------------------- */
 angular.module('CatClicker')
     .controller('listCtrl', function() {
-
+        this.cats = [
+            { name: 'Pixel', src: 'img/cat-01.jpg', count: 5 },
+            { name: 'Garfield', src: 'img/cat-02.jpg', count: 1 },
+            { name: 'Bichento', src: 'img/cat-03.jpg', count: 3 },
+            { name: 'Tom', src: 'img/cat-04.jpg', count: 0 },
+            { name: 'Buttercup', src: 'img/cat-05.jpg', count: 3 },
+        ];
     });
     
