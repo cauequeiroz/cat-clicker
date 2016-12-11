29aafe05ef89ac48a8ae84431b20148783e81bff
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-11 09:19:57 -0200

    Create catmodel service

--- angularjs/js/controller/cat-list.js
+++ angularjs/js/controller/cat-list.js
@@ -6,13 +6,8 @@
 
 -------------------------------------------- */
 angular.module('CatClicker')
-    .controller('listCtrl', function() {
-        this.cats = [
-            { name: 'Pixel', src: 'img/cat-01.jpg', count: 5 },
-            { name: 'Garfield', src: 'img/cat-02.jpg', count: 1 },
-            { name: 'Bichento', src: 'img/cat-03.jpg', count: 3 },
-            { name: 'Tom', src: 'img/cat-04.jpg', count: 0 },
-            { name: 'Buttercup', src: 'img/cat-05.jpg', count: 3 },
-        ];
-    });
+
+.controller('listCtrl', ['catModel', function(model) {
+    this.cats = model.getAllCats();
+}]);
     
---
+++ angularjs/js/service/cat-model.js
@@ -0,0 +1,22 @@
+/* -------------------------------------------
+
+    Cat Clicker
+    @AngularJS Version
+    @Developed by Caue Queiroz	
+
+-------------------------------------------- */
+angular.module('CatClicker')
+
+.service('catModel', function() {
+    this.cats = [
+        { name: 'Pixel', src: 'img/cat-01.jpg', count: 5 },
+        { name: 'Garfield', src: 'img/cat-02.jpg', count: 1 },
+        { name: 'Bichento', src: 'img/cat-03.jpg', count: 3 },
+        { name: 'Tom', src: 'img/cat-04.jpg', count: 0 },
+        { name: 'Buttercup', src: 'img/cat-05.jpg', count: 3 },
+    ];
+
+    this.getAllCats = function() {
+        return this.cats;
+    };
+});
