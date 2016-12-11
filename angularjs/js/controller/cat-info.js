3650ac13c400308847e942d2da25b0aab2f41250
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-11 09:53:23 -0200

    create cat info controller

---
+++ angularjs/js/controller/cat-info.js
@@ -0,0 +1,12 @@
+/* -------------------------------------------
+
+    Cat Clicker
+    @AngularJS Version
+    @Developed by Caue Queiroz  
+
+-------------------------------------------- */
+angular.module('CatClicker')
+
+.controller('infoCtrl', ['catModel', function(model) {
+
+}]);
