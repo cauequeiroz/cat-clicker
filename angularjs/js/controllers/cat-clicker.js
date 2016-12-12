d96b13c775390e40ec2a304a26fc62ffb430f814
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-12 09:14:49 -0200

    Add some cats and functions to controller

---
+++ angularjs/js/controllers/cat-clicker.js
@@ -0,0 +1,33 @@
+/* -----------------------------------------------
+
+    @Controller
+    Control Cat Clicker general functions.
+
+----------------------------------------------- */
+angular.module('CatClicker')
+
+.controller('CatClickerCtrl', function() {
+
+    // Generate some cats
+    // ------------------------------------------
+    this.catList = [
+        { name: 'Pixel', src: 'img/cat-01.jpg', count: 5 },
+        { name: 'Garfield', src: 'img/cat-02.jpg', count: 1 },
+        { name: 'Bichento', src: 'img/cat-03.jpg', count: 3 },
+        { name: 'Tom', src: 'img/cat-04.jpg', count: 0 },
+        { name: 'Buttercup', src: 'img/cat-05.jpg', count: 3 },
+    ];
+
+    this.currentCat = this.catList[0];
+
+    // Control functions
+    // ------------------------------------------
+    this.getCat = function() {
+        return this.currentCat;
+    };
+
+    this.setCat = function(index) {
+        this.currentCat = this.catList[index];
+    };
+
+});
